99f08ebcaa991d7ae4fb75a7c114bb0a957cfa31
Christian | Cy <cvillasenor@medgrocer.com>
2025-03-06 14:35:16 +0800

    test merge restrictions

--- src/theme.js
+++ src/theme.js
@@ -1,16 +1,23 @@
 import { extendTheme } from "@chakra-ui/react";
+import { color } from "framer-motion";
 
 const theme = extendTheme({
-  semanticTokens: {
-    colors: {
-      error: 'red.500',
-      text: {
-        default: '#42adf5',
-        secondary: '#730000',
-        white: '#fff',
-        _dark: '#730000',
-      },
+  colors: {
+    error: 'red.500',
+    text: {
+      default: '#42adf5',
+      secondary: '#730000',
+      white: '#fff',
+      _dark: '#730000',
     },
+  },
+  breakpoints: {
+    base: '0em',
+    sm: '30em',
+    md: '48em',
+    lg: '62em',
+    xl: '80em',
+    '2xl': '96em',
   },
 })
 
